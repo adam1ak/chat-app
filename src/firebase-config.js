19239035fe0c57d8e39c94d54de4a6d59a7e1858
adam1ak <f.admiakk@gmail.com>
2024-11-01 17:18:59 +0100

    hide .env log

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,15 +3,6 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore/lite';
 
-console.log("API Key: ", process.env.REACT_APP_FIREBASE_API_KEY);
-console.log("Auth Domain: ", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-console.log("Database URL: ", process.env.REACT_APP_FIREBASE_DATABASE_URL);
-console.log("Project ID: ", process.env.REACT_APP_FIREBASE_PROJECT_ID);
-console.log("Storage Bucket: ", process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
-console.log("Messaging Sender ID: ", process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
-console.log("App ID: ", process.env.REACT_APP_FIREBASE_APP_ID);
-
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
