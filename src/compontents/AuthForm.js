03fd634f2d272f1467bd7398a134010b6b3044ac
adam1ak <f.admiakk@gmail.com>
2024-11-03 21:34:37 +0100

    added theme functionality

--- src/compontents/AuthForm.js
+++ src/compontents/AuthForm.js
@@ -7,8 +7,11 @@ import app from '../firebase-config.js';
 
 import '../styles/auth-form.css'
 import logo from '../assets/logo.svg'
+import logo_light from '../assets/logo_light.svg'
 import logoMax from '../assets/logoMax.svg'
+import logoMax_light from '../assets/logoMax_light.svg'
 import lightbulb from '../assets/lightbulb.svg'
+import lightbulb_light_mode from '../assets/lightbulb_light_mode.svg'
 import passShow from '../assets/passShow.svg'
 
 function AuthForm() {
@@ -18,15 +21,38 @@ function AuthForm() {
 
     const [mode, setMode] = useState('register');
     const [web, setWeb] = useState(false);
+    const [bulbSrc, setBulbSrc] = useState(lightbulb);
     const [logoSrc, setLogoSrc] = useState(logo);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
+
+    useEffect(() => {
+        document.body.setAttribute("data-theme", theme)
+        localStorage.setItem("theme", theme);
+        if(theme === 'light') {
+            setBulbSrc(lightbulb_light_mode)
+            if(web) {
+                setLogoSrc(logoMax_light)
+            }else{
+                setLogoSrc(logo_light)
+            }
+        }
+    }, [theme]);
 
     useEffect(() => {
         const handleResize = () => {
             if (window.innerWidth >= 992 && !web) {
-                setLogoSrc(logoMax)
+                if (theme === 'light') {
+                    setLogoSrc(logoMax_light)
+                } else {
+                    setLogoSrc(logoMax)
+                }
                 setWeb(true);
             } else if (window.innerWidth <= 992 && web) {
-                setLogoSrc(logo)
+                if (theme === 'light') {
+                    setLogoSrc(logo_light)
+                } else {
+                    setLogoSrc(logo)
+                }
                 setWeb(false);
             }
         };
@@ -50,6 +76,18 @@ function AuthForm() {
         
         return () => {};
       }, [auth, navigate]);
+
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light")
+        setBulbSrc(theme === "light" ? lightbulb : lightbulb_light_mode)
+        if(web) {
+            setLogoSrc(theme === "light" ? logoMax : logoMax_light)
+        }else{
+            setLogoSrc(theme === "light" ? logo : logo_light)
+        }
+
+    }
+
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -113,16 +151,18 @@ function AuthForm() {
             <div className="auth-form__top-container">
                 <img src={logoSrc} alt="logo" className='auth-form_logo' />
                 <button
-                    className="auth-form_light-mode-btn">
-                    <img src={lightbulb} alt="lightbulb" className="auth-form_lightbulb" />
+                    className="auth-form_light-mode-btn"
+                    onClick={toggleTheme}>
+                    <img src={bulbSrc} alt="lightbulb" className="auth-form_lightbulb" />
                 </button>
             </div>
             <div className='auth-form'>
                 <div className="auth-form__header-container">
                     <div className="auth-form_header">
                         <button
-                            className="auth-form_light-mode-btn">
-                            <img src={lightbulb} alt="lightbulb" className="auth-form_lightbulb" />
+                            className="auth-form_light-mode-btn"
+                            onClick={toggleTheme}>
+                            <img src={bulbSrc} alt="lightbulb" className="auth-form_lightbulb" />
                         </button>
                         <h1>Sign {mode === 'register' ? 'Up' : 'In'}</h1>
                         {mode === 'register' && (
